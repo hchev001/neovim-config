ef87094a035c7d519bff5ed7162547c4e2bb49e0
Hamilton Chevez <hchevez@bgprod.com>
2024-06-02 22:44:58 -0400

    Installed various treesitter parsers and added readme.md

---
+++ README.md
@@ -0,0 +1,13 @@
+## NeoVIM config by Hamilton Chevez
+
+### Dependencies
+Git
+Nvim
+a C compiler
+
+### Setup
+#### Windows
+- Install chocolatey
+- Run `choco install mingw` in an admin powershell
+- Run `refreshenv` or open a new shell
+- Start Neovim
--- lua/hacdev/plugins/treesitter.lua
+++ lua/hacdev/plugins/treesitter.lua
@@ -1,6 +1,16 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = function()
-    require("nvim-treesitter.install").update({ with_sync = true })()
+  build = ":TSUpdate",
+  config = function ()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "c_sharp", "css", "csv", "go", "graphql", "haskell", "java", "jsdoc", "json", "regex", "scala", "scss", "sql","svelte", "tsx" },
+      sync_install = false, -- install parsers synchronously, applies to ensure_installed
+      auto_install = false,
+      highlight = {
+        enable = true
+      }
+    })
   end
 }
