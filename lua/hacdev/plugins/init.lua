c62ccfd5e71c63ae178c1b32b5b6fba75d60ef71
Hamilton Chevez <hchev001@gmail.com>
2024-06-02 23:58:56 -0400

    added telescope and which-key

--- lua/hacdev/plugins/init.lua
+++ lua/hacdev/plugins/init.lua
@@ -1,7 +1,3 @@
-require("hacdev/plugins/bufferline")
-require("hacdev/plugins/colorscheme")
-require("hacdev/plugins/neo-tree")
-require("hacdev/plugins/treesitter")
 
 return {
 	"nvim-lua/plenary.nvim" -- lua funcitons that many plugins use
