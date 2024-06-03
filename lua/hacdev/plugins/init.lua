d92036c700867abac422999409d0bfdfe6f22243
Hamilton Chevez <hchevez@bgprod.com>
2024-06-02 22:06:32 -0400

    added treesitter

--- lua/hacdev/plugins/init.lua
+++ lua/hacdev/plugins/init.lua
@@ -1,3 +1,8 @@
+require("hacdev/plugins/bufferline")
+require("hacdev/plugins/colorscheme")
+require("hacdev/plugins/neo-tree")
+require("hacdev/plugins/treesitter")
+
 return {
 	"nvim-lua/plenary.nvim" -- lua funcitons that many plugins use
 }
