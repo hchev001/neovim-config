ad28d2bb73a6803422a92c02e65bff777a25769d
Hamilton Chevez <hchev001@gmail.com>
2024-06-04 01:09:46 -0400

    added lsp, cmp, todo-comment, comment, autopairs, indent-line

--- lua/hacdev/lazy.lua
+++ lua/hacdev/lazy.lua
@@ -11,4 +11,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("hacdev.plugins")
+require("lazy").setup({ { import = "hacdev.plugins" }, { import = "hacdev.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false
+  },
+})
